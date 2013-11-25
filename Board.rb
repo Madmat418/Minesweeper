f836ed668988981f94a3dff3cfabfee590dc49fa
App Academy Student <student@appacademy.io>
2013-11-25 14:51:23 -0800

    Misc

--- Board.rb
+++ Board.rb
@@ -8,7 +8,7 @@ class Board
 
   def create_board(num_bombs, size)
     tiles = []
-    Array.new(size) do |row|
+    board = Array.new(size) do |row|
       Array.new(size) do |col|
         tile = Tile.new([row, col])
         tiles << tile
@@ -19,15 +19,18 @@ class Board
     tiles.sample(num_bombs).each do |tile|
       tile.is_bomb = true
     end
+    board
   end
 
-  def get_neighbors(tile_position)
+
+
+  def get_neighbors(tile)
     neighbor_tiles = []
     (-1..1).each do |x|
       (-1..1).each do |y|
         next if x == 0 && y == 0
-        new_tile_x = tile_position[0] + x
-        new_tile_y = tile_position[1] + y
+        new_tile_x = tile.position[0] + x
+        new_tile_y = tile.position[1] + y
         next if new_tile_x < 0 || new_tile_x > 8
         next if new_tile_y < 0 || new_tile_y > 8
         neighbor_tiles << @board_status[new_tile_x][new_tile_y]
@@ -36,8 +39,29 @@ class Board
     neighbor_tiles
   end
 
+  def neighbor_bomb_count(neighbors)
+    bomb_count = 0
+    neighbors.each do |tile|
+      bomb_count += 1 if tile.is_bomb
+    end
+    bomb_count
+  end
+
   def reveal(tile)
-    tile.reveal = true
+    return if tile.is_revealed
+    tile.is_revealed = true
+    neighbors = get_neighbors(tile)
+    bomb_count = neighbor_bomb_count
+    if tile.is_bomb
+      #Boom
+    elsif bomb_count > 0
+      bomb_count(neighbors)
+    else
+      neighbors.each do |neighbor|
+        reveal(neighbor)
+      end
+    else
+
   end
 
   def flag(tile)
